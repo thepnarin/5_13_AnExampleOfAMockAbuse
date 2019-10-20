e826e6f6b3f6a0a28989be0280a6f8e4b681a22d
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 14:15:49 +0700

    AnExampleOfAMockAbuse

---
+++ 5_13_AnExampleOfAMockAbuse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_13_AnExampleOfAMockAbuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TestNinja.UnitTests/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using _5_13_AnExampleOfAMockAbuse.Mocking;
+using Moq;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount2()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(c => c.IsGold).Returns(true);
+
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+
+    }
+}
